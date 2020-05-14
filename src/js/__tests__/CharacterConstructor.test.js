b43d96f97542e73805c06d348d16645a375e0f97
Комиссарова Татьяна <nebykova@office.local>
2020-05-14 18:05:38 +0300

    eslint fixes in tests

--- src/js/__tests__/CharacterConstructor.test.js
+++ src/js/__tests__/CharacterConstructor.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import Character from '../CharacterConstructor';
 
@@ -15,21 +16,18 @@ test('возвращает созданного персонажа', () => {
 });
 
 test('возвращает ошибку из-за несуществующего типа', () => {
-  expect(() => 
-  {const myWitcher = new Character('Геральт', 'Witcher');})
-  .toThrowError(new Error('Несуществующий тип персонажа!'));
+  expect(() => { const myWitcher = new Character('Геральт', 'Witcher'); })
+    .toThrowError(new Error('Несуществующий тип персонажа!'));
 });
 
 
 test('возвращает ошибку из-за длины имени', () => {
-
   expect(() => {
     const myDaemon = new Character('Ио', 'Daemon');
   }).toThrowError(new Error('Имя персонажа должно быть не менее 2 и не не более 10 символов в длину'));
 });
 
 test('возвращает ошибку из-за формата ввода', () => {
-
   expect(() => {
     const myUndead = new Character(30, 40);
   }).toThrowError(new Error('Ошибка формата'));
