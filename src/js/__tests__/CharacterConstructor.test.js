922541ee7ae4baa3b95bbf246b85eae41ffd2243
Комиссарова Татьяна <nebykova@office.local>
2020-05-14 17:58:21 +0300

    fixes in tests

--- src/js/__tests__/CharacterConstructor.test.js
+++ src/js/__tests__/CharacterConstructor.test.js
@@ -15,20 +15,22 @@ test('возвращает созданного персонажа', () => {
 });
 
 test('возвращает ошибку из-за несуществующего типа', () => {
-  const myWitcher = new Character('Геральт', 'Witcher');
-
-  expect(myWitcher).toThrowError(new Error('Несуществующий тип персонажа!'));
+  expect(() => 
+  {const myWitcher = new Character('Геральт', 'Witcher');})
+  .toThrowError(new Error('Несуществующий тип персонажа!'));
 });
 
 
 test('возвращает ошибку из-за длины имени', () => {
-  const myDaemon = new Character('Ио', 'Daemon');
 
-  expect(myDaemon).toThrowError(new Error('Имя персонажа должно быть не менее 2 и не не более 10 символов в длину'));
+  expect(() => {
+    const myDaemon = new Character('Ио', 'Daemon');
+  }).toThrowError(new Error('Имя персонажа должно быть не менее 2 и не не более 10 символов в длину'));
 });
 
 test('возвращает ошибку из-за формата ввода', () => {
-  const myUndead = new Character(30, 40);
 
-  expect(myUndead).toThrowError(new Error('Ошибка формата'));
+  expect(() => {
+    const myUndead = new Character(30, 40);
+  }).toThrowError(new Error('Ошибка формата'));
 });
